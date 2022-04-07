6f0e61d229862b8736728fe8471dee9d0aaca2ce
Ledmington <ledmington.dev@gmail.com>
2022-04-07 17:24:16 +0200

    Minor fix in test_one_dead_peer.py

--- ddml/tests/peers/test_one_dead_peer.py
+++ ddml/tests/peers/test_one_dead_peer.py
@@ -1,4 +1,5 @@
 import os
+import pytest
 from testcontainers.compose import DockerCompose
 from testcontainers.core.waiting_utils import wait_for_logs
 
@@ -7,13 +8,12 @@ from ddml.peers.peer import Peer
 
 
 def test_one_dead_peer():
-    p = Peer()  # TODO: change this code in order to read simple constants
-    time_to_wait = p.seconds_to_be_dead + p.max_seconds_without_answers
-    p.die()
-
     compose_file = "test_one_dead_peer.yml"
 
     with DockerCompose(
         os.sep.join([ROOT_DIR, "tests", "peers"]), compose_file_name=compose_file
     ) as compose:
-        wait_for_logs(compose, r".* is dead")
+        try:
+            wait_for_logs(compose, r"is dead", timeout=120)
+        except TimeoutError:
+            pytest.fail()
