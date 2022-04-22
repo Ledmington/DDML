19544c460900e59c59d61bdad9d072b31c77188d
Ledmington <ledmington.dev@gmail.com>
2022-04-22 16:09:57 +0200

    Added command line arguments

--- ddml/main.py
+++ ddml/main.py
@@ -1,6 +1,6 @@
 import sys
+import argparse
 
-from ddml.peers.peer import Peer
 from ddml.utils.colors import colored
 
 
@@ -10,28 +10,67 @@ def print_version():
     print("ddml-peer v" + ddml.__version__)
 
 
-if __name__ == "__main__":
-    interactive_mode = False
-    for arg in sys.argv[1:]:
-        if arg in ["-i", "--interactive"]:
-            interactive_mode = True
-        elif arg in ["-v", "--version"]:
-            print_version()
-            sys.exit(0)
-        else:
-            print(colored("ERROR", "red"), f': unknown argument "{arg}"')
+def set_interactive_mode(config: dict):
+    config["interactive"] = True
 
+
+def main(arguments):
     print_version()
 
-    if interactive_mode is True:
+    if arguments.interactive is True:
         from ddml.peers.interactive_peer import InteractivePeer
 
         print("Starting an interactive peer")
+        print("============================")
         p = InteractivePeer()
         p.start()
         p.join()
     else:
+        from ddml.peers.peer import Peer
+
         print("Starting a non-interactive peer")
+        print("===============================")
         p = Peer()
         p.start()
         p.join()
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="A peer designed to train machine learning models in a decentralized and distributed manner.",
+        allow_abbrev=False,
+        add_help=False,
+    )
+    parser.add_argument(
+        "-h",
+        "--help",
+        default=None,
+        action="store_true",
+        help="Print this help message and exit",
+    )
+    parser.add_argument(
+        "-v",
+        "--version",
+        default=None,
+        action="store_true",
+        help="Print version number and exit",
+    )
+    parser.add_argument(
+        "-i",
+        "--interactive",
+        default=None,
+        action="store_true",
+        help="Enable interactive mode",
+    )
+
+    args = parser.parse_args()
+
+    if args.help is not None:
+        print_version()
+        parser.print_help()
+        sys.exit(0)
+    elif args.version is not None:
+        print_version()
+        sys.exit(0)
+
+    main(args)
