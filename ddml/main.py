e3ef5f561400794dcac4007f3f5198afa03a47bc
Ledmington <ledmington.dev@gmail.com>
2022-04-22 15:03:14 +0200

    Added print_version

--- ddml/main.py
+++ ddml/main.py
@@ -3,13 +3,25 @@ import sys
 from ddml.peers.peer import Peer
 from ddml.utils.colors import colored
 
+
+def print_version():
+    import ddml
+
+    print("ddml-peer v" + ddml.__version__)
+
+
 if __name__ == "__main__":
     interactive_mode = False
     for arg in sys.argv[1:]:
         if arg in ["-i", "--interactive"]:
             interactive_mode = True
+        elif arg in ["-v", "--version"]:
+            print_version()
+            sys.exit(0)
         else:
             print(colored("ERROR", "red"), f': unknown argument "{arg}"')
+
+    print_version()
 
     if interactive_mode is True:
         from ddml.peers.interactive_peer import InteractivePeer
