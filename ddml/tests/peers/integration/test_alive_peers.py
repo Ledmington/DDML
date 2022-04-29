90a4e578c6e515b1a0f3e9ee81d3b8e0cea5acab
Ledmington <ledmington.dev@gmail.com>
2022-04-29 13:59:07 +0200

    Added new integration test: test_alive_peers

--- ddml/tests/peers/integration/test_alive_peers.py
+++ ddml/tests/peers/integration/test_alive_peers.py
@@ -6,15 +6,27 @@ from testcontainers.core.waiting_utils import wait_for_logs
 from ddml import ROOT_DIR
 
 
-@pytest.mark.skip  # uncomment to avoid slow integration testing
+# @pytest.mark.skip  # uncomment to avoid slow integration testing
 def test_alive_peers():
     compose_file = "test_alive_peers.yml"
+
+    n_peers = 4
+
+    print(f"Creating {n_peers} peers")
 
     with DockerCompose(
         os.sep.join([ROOT_DIR, "tests", "peers", "integration"]),
         compose_file_name=compose_file,
     ) as compose:
-        try:
-            wait_for_logs(compose, "is dead", timeout=120)
-        except TimeoutError:
-            pytest.fail()
+        import time
+
+        time.sleep(60)
+        stdout, stderr = compose.get_logs()
+
+    lines = stdout.decode().split("\n")
+    peers_logs = (
+        filter(lambda s: "peer-" + str(i + 1) in s, lines) for i in range(n_peers)
+    )
+
+    for log in peers_logs:
+        assert 0 < len([filter(lambda s: str(n_peers - 1) + " other peers", log)])
